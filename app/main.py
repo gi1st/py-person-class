72cee3989774b5bdcee4fe7e2895568da9ae95f1
Yakiv Seleznov <yakov.seleznyov@gmail.com>
2025-05-09 18:19:51 +0200

    Solve init atributs

--- app/main.py
+++ app/main.py
@@ -4,16 +4,24 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
+        self.wife = None
+        self.husband = None
         Person.people[self.name] = self
 
 
-def create_person_list(people: list) -> list:
-    persons = [Person(person["name"], person["age"]) for person in people]
-    for i in range(len(people)):
-        if "wife" in people[i] and people[i]["wife"] is not None:
-            wife = people[i].get("wife")
-            persons[i].wife = Person.people[wife]
-        elif "husband" in people[i] and people[i]["husband"] is not None:
-            husband = people[i].get("husband")
-            persons[i].husband = Person.people[husband]
+def create_person_list(data: list) -> list:
+    persons = [Person(person["name"], person["age"]) for person in data]
+    for i in range(len(data)):
+        if "wife" in data[i]:
+            if data[i]["wife"] is not None:
+                wife = data[i].get("wife")
+                persons[i].wife = Person.people[wife]
+            else:
+                delattr(persons[i], "wife")
+        elif "husband" in data[i]:
+            if data[i]["husband"] is not None:
+                husband = data[i].get("husband")
+                persons[i].husband = Person.people[husband]
+            else:
+                delattr(persons[i], "husband")
     return persons

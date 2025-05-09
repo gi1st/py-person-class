4f6d78f29c441a8e4c3c0bc4853186d1ddffb1d7
Yakiv Seleznov <yakov.seleznyov@gmail.com>
2025-05-09 18:43:39 +0200

    Done PR requrenments

--- app/main.py
+++ app/main.py
@@ -13,15 +13,15 @@ def create_person_list(data: list) -> list:
     persons = [Person(person["name"], person["age"]) for person in data]
     for i in range(len(data)):
         if "wife" in data[i]:
-            if data[i]["wife"] is not None:
+            if data[i]["wife"] is not None and data[i]["wife"] in Person.people:            
                 wife = data[i].get("wife")
                 persons[i].wife = Person.people[wife]
             else:
-                delattr(persons[i], "wife")
+                persons[i].wife = None
         elif "husband" in data[i]:
-            if data[i]["husband"] is not None:
+            if data[i]["husband"] is not None and data[i]["husband"] in Person.people:
                 husband = data[i].get("husband")
                 persons[i].husband = Person.people[husband]
             else:
-                delattr(persons[i], "husband")
+                persons[i].husband = None
     return persons
